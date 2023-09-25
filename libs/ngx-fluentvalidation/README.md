fd691f2399232b38f641497f6feba332cbafc316
bohoffi <bo.hoffi@gmail.com>
2023-09-25 02:04:57 +0200

    doc: updated docs and next steps

--- libs/ngx-fluentvalidation/README.md
+++ libs/ngx-fluentvalidation/README.md
@@ -2,11 +2,47 @@
 
 - [x] base validator implementation
   - [x] set of rules for non iterable properties
-  - [ ] set of rules for iterable properties
   - [x] pass a validator as a rule
-  - [ ] pass an AbstractValidator instance for a property
-  - [ ] inline validator creation
-  - [ ] Rule set like in .NET
-  - [ ] valiadate options to validate specific subset
-  - [ ] Cascading of rules (break on first fail)
 - [x] base rule implementation
+
+## Check and Fix
+
+- Restructure code in new TS lib project `ts-fluentvalidation`
+- Rules
+  - [ ] `setValidator` should be apllicable to all property types - not just object types
+
+## Extension - Phase I
+
+- Validator
+  - [ ] set of rules for iterable properties
+  - [ ] [Cascading of rules](https://docs.fluentvalidation.net/en/latest/cascade.html) (break on first fail)
+- Rules
+  - Cross-property comparison rules (add possibility to compare against another property value)
+    - [ ] notEqual
+    - [ ] equal
+    - [ ] lessThan
+    - [ ] lessThanOrEqualTo
+    - [ ] greaterThan
+    - [ ] greaterThanOrEqualTo
+  - add `empty` rule `length` and `size` properties
+
+## Extension - Phase II
+
+- Validator
+  - [ ] [Rule sets](https://docs.fluentvalidation.net/en/latest/rulesets.html) like in .NET
+  - [ ] validate options to validate [specific subset](https://docs.fluentvalidation.net/en/latest/specific-properties.html)
+  - [ ] inline validator creation
+
+## Extension - Phase III
+
+- Validator
+  - [ ] [Async validation](https://docs.fluentvalidation.net/en/latest/async.html)
+  - [ ] [Include](https://docs.fluentvalidation.net/en/latest/including-rules.html) another validator to reuse set rules
+
+## Extension - Phase IV
+
+- Validators
+  - [ ] [Inheritance validation](https://docs.fluentvalidation.net/en/latest/inheritance.html)
+- Rules
+  - Conditions
+    - [ ] [Otherwise](https://docs.fluentvalidation.net/en/latest/conditions.html)
--- libs/ngx-fluentvalidation/src/lib/core/validators/typed/typed-validators.ts
+++ libs/ngx-fluentvalidation/src/lib/core/validators/typed/typed-validators.ts
@@ -1,62 +1,163 @@
 import { ApplyConditionTo } from '../../types';
 import { IValidator } from '../interfaces';
 
-export type StringProperty = string | null | undefined;
-export type NumberProperty = number | bigint | null | undefined;
-export type ObjectProperty = object | null | undefined;
-export type LengthProperty = { length: number };
+type StringProperty = string | null | undefined;
+type NumberProperty = number | bigint | null | undefined;
+type ObjectProperty = object | null | undefined;
+type LengthProperty = { length: number };
 
-export type CommonValidator<TModel, TProperty> = {
+type CommonValidator<TModel, TProperty> = {
   /**
-   * Validates that the value should be null.
+   * Defines a `null` validation.
+   *
+   * Validation will fail if the value is not null.
    */
   isNull(): ExtendedValidator<TModel, TProperty>;
   /**
-   * Validates that the value should not be null.
+   * Defines a `not null` validation.
+   *
+   * Validation will fail if the value is null.
    */
   notNull(): ExtendedValidator<TModel, TProperty>;
   /**
-   * Validates that the value should be equal to a reference value.
+   * Defines an `equal` validation.
+   *
+   * Validation will fail if the value is not equal to the given reference value.
+   * @param referenceValue The value to compare against
    */
   equal(referenceValue: TProperty): ExtendedValidator<TModel, TProperty>;
   /**
-   * Validates that the value should not be equal to a reference value.
+   * Defines a `not equal` validation.
+   *
+   * Validation will fail if the value is equal to the given reference value.
+   * @param referenceValue The value to compare against
    */
   notEqual(referenceValue: TProperty): ExtendedValidator<TModel, TProperty>;
+  /**
+   * Defines a predicate validation.
+   *
+   * Validation will fail if the value does not meet the given condition.
+   * @param predicate Condition to check against
+   */
   must(predicate: (value: TProperty | null | undefined, model: TModel) => boolean): ExtendedValidator<TModel, TProperty>;
 };
 
-export type StringValidator<TModel, TProperty extends StringProperty> = {
+type StringValidator<TModel, TProperty extends StringProperty> = {
+  /**
+   * Defines a credit card number validation. This validation uses the Luhn algorythm.
+   *
+   * Validation will fail if the value is not a valid credit card number.
+   */
   creditCard(): ExtendedValidator<TModel, TProperty>;
+  /**
+   * Defines an `empty` validation.
+   *
+   * Validation will fail if the value is not an empty string.
+   */
   empty(): ExtendedValidator<TModel, TProperty>;
   /**
-   * Validates that the value should not be an empty string.
+   * Defines a `not empty` validation.
+   *
+   * Validation will fail if the value is an empty string.
    */
   notEmpty(): ExtendedValidator<TModel, TProperty>;
+  /**
+   * Defines a regular expression validation.
+   *
+   * Validation will fail if the given regular expression tests negative against the value.
+   * @param regExp Regular expression to test against the value
+   */
   matches(regExp: RegExp): ExtendedValidator<TModel, TProperty>;
 };
 
-export type NumberValidator<TModel, TProperty extends NumberProperty> = {
+type NumberValidator<TModel, TProperty extends NumberProperty> = {
+  /**
+   * Defines a `less than` validation.
+   *
+   * Validation will fail if the value is equal to or greater than the given reference value.
+   * @param referenceValue The value to compare against
+   */
   lessThan(referenceValue: number): ExtendedValidator<TModel, TProperty>;
+  /**
+   * Defines a `less than or equal to` validation.
+   *
+   * Validation will fail if the value is greater than the given reference value.
+   * @param referenceValue The value to compare against
+   */
   lessThanOrEqualTo(referenceValue: number): ExtendedValidator<TModel, TProperty>;
+  /**
+   * Defines a `greater than` validation.
+   *
+   * Validation will fail if the value is equal to or less than the given reference value.
+   * @param referenceValue The value to compare against
+   */
   greaterThan(referenceValue: number): ExtendedValidator<TModel, TProperty>;
+  /**
+   * Defines a `greater than or equals` validation.
+   *
+   * Validation will fail if the value is less than the given reference value.
+   * @param referenceValue The value to compare against
+   */
   greaterThanOrEqualTo(referenceValue: number): ExtendedValidator<TModel, TProperty>;
   /**
-   * Validates that the value should be a positive number.
+   * Defines a `is positive` validation.
+   *
+   * Validation will fail if the value less than `1`.
    */
   isPositive(): ExtendedValidator<TModel, TProperty>;
+  /**
+   * Defines a `is negative` validation.
+   *
+   * Validation will fail if the value greater than `-1`.
+   */
   isNegative(): ExtendedValidator<TModel, TProperty>;
+  /**
+   * Defines an `exclusive between` validation.
+   *
+   * Validation will fail if the value is not inside the given range - `min/max` are not part of the range.
+   * @param options The options specifying the range to check against
+   */
   exclusiveBetween(options: { min: number; max: number }): ExtendedValidator<TModel, TProperty>;
+  /**
+   * Defines an `inclusive between` validation.
+   *
+   * Validation will fail if the value is not inside the given range - `min/max` are part of the range.
+   * @param options The options specifying the range to check against
+   */
   inclusiveBetween(options: { min: number; max: number }): ExtendedValidator<TModel, TProperty>;
 };
 
-export type ObjectValidator<TModel, TProperty extends ObjectProperty> = {
+type ObjectValidator<TModel, TProperty extends ObjectProperty> = {
+  /**
+   * Defines a validation against a specific validator.
+   *
+   * Validation will fail if the given validator fails.
+   * @param validator Specific validator to perform validations on the value.
+   */
   setValidator(validator: IValidator<TProperty>): ExtendedValidator<TModel, TProperty>;
 };
 
-export type LengthValidator<TModel, TProperty extends LengthProperty> = {
+type LengthValidator<TModel, TProperty extends LengthProperty> = {
+  /**
+   * Defines a `min/max length` validation.
+   *
+   * Validation will fail if the length of the value is either less then the minLength or greater then maxLength.
+   * @param options The options specifying the range to check against
+   */
   length(options: { minLength: number; maxLength: number }): ExtendedValidator<TModel, TProperty>;
+  /**
+   * Defines a 'max length' validation.
+   *
+   * Validation will fail if the length of the value is greater than the given maxLength.
+   * @param maxLength The maximum length to check against
+   */
   maxLength(maxLength: number): ExtendedValidator<TModel, TProperty>;
+  /**
+   * Defines a 'min length' validation.
+   *
+   * Validation will fail if the length of the value is less than the given minLength.
+   * @param maxLength The minimum length to check against
+   */
   minLength(minLength: number): ExtendedValidator<TModel, TProperty>;
 };
 
@@ -66,12 +167,30 @@ export type TypeValidator<TModel, TProperty> = CommonValidator<TModel, TProperty
   (TProperty extends ObjectProperty ? ObjectValidator<TModel, TProperty> : unknown) &
   (TProperty extends LengthProperty ? LengthValidator<TModel, TProperty> : unknown);
 
-export type ConditionalValidator<TModel, TProperty> = TypeValidator<TModel, TProperty> & {
+type ConditionalValidator<TModel, TProperty> = TypeValidator<TModel, TProperty> & {
+  /**
+   * Defines a condition when a validation rule should execute. Rule will not get executed if the condition evaluates to `false`.
+   * @param condition Condition to evaluate
+   * @param applyConditionTo Option to apply condition to either all preceding validation rules - the default - or to the last validation rule only.
+   */
   when(condition: (model: TModel) => boolean, applyConditionTo?: ApplyConditionTo): TypeValidator<TModel, TProperty>;
+  /**
+   * Defines a condition when a validation rule should execute. Rule will not get executed if the condition evaluates to `true`.
+   * @param condition Condition to evaluate
+   * @param applyConditionTo Option to apply condition to either all preceding validation rules - the default - or to the last validation rule only.
+   */
   unless(condition: (model: TModel) => boolean, applyConditionTo?: ApplyConditionTo): TypeValidator<TModel, TProperty>;
 };
 
-export type ExtendedValidator<TModel, TProperty> = ConditionalValidator<TModel, TProperty> & {
+type ExtendedValidator<TModel, TProperty> = ConditionalValidator<TModel, TProperty> & {
+  /**
+   * Overrides the default error message of a validation rule.
+   * @param message Custom error message to use
+   */
   withMessage(message: string): ConditionalValidator<TModel, TProperty>;
+  /**
+   * Overrides the default property name of a validation rule.
+   * @param message Custom property name to use
+   */
   withName(propertyName: string): ConditionalValidator<TModel, TProperty>;
 };
